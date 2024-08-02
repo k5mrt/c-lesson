0f1d1b87c0f02a51451081ee01cb89eb265e3566
k5mrt <85227053+k5mrt@users.noreply.github.com>
2024-08-03 01:29:41 +0900

    green

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -5,11 +5,49 @@
 cc cl_getc.c int_parser_getc.c
 */
 
+const char ascii_numbers[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
 enum Type {
     NUMBER,
     SPACE
 };
 char parse_one(int last_c, int *out_val, enum Type *out_type) {
+    int val;
+    int type;
+
+    if(last_c == ' ') {
+        while(last_c == ' ') {
+            last_c = cl_getc();
+        }
+        type = SPACE;
+        val = ' ';
+    } else {
+        val = 0;
+        for(int i = 0; i < 10; i++) {
+            if(last_c == ascii_numbers[i]) {
+                val = i;
+                break;
+            }
+        }
+        while(last_c != ' ') {
+            last_c = cl_getc();
+            if (last_c == EOF) {
+                break;
+            }
+            for(int i = 0; i < 10; i++) {
+                if(last_c == ascii_numbers[i]) {
+                    val = val * 10 + i;
+                    break;
+                }
+            }
+        }
+        type = NUMBER;
+    }
+
+    *out_val = val;
+    *out_type = type;
+
+    return last_c;
 }
 
 int main() {
@@ -17,6 +55,23 @@ int main() {
     int answer2 = 0;
 
     // write something here.
+
+    int answers[2] = {0, 0};
+    int answer_index = 0;
+
+    int next_c = cl_getc();
+    while(next_c != EOF) {
+        int out_val;
+        enum Type out_type;
+        next_c = parse_one(next_c, &out_val, &out_type);
+        if(out_type == NUMBER) {
+            answers[answer_index] = out_val;
+            answer_index++;
+        }
+    }
+
+    answer1 = answers[0];
+    answer2 = answers[1];
 
     // sample for cl_getc() usage.
     int c;
